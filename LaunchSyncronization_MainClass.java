cddba3a467335b0c1dc55ed390e16b065456b338
ZeusSR <89997443+ZeusSR@users.noreply.github.com>
2022-10-26 11:27:43 +0530

    Add files via upload

---
+++ LaunchSyncronization_MainClass.java
@@ -0,0 +1,45 @@
+/*
+ Here it is an example of synchronization which is  by the keyword synchronized */
+package ThreadPackage;
+
+class Atm implements Runnable
+{
+	
+	synchronized public void run()
+	{
+		try {
+		System.out.println(Thread.currentThread().getName()+" has entered the ATM");
+		Thread.sleep(5000);
+		System.out.println(Thread.currentThread().getName()+" is using the ATM");
+		Thread.sleep(5000);
+		System.out.println(Thread.currentThread().getName()+" has left the ATM");
+		Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+	}
+}
+
+public class LaunchSyncronization_MainClass {
+
+	public static void main(String[] args) {
+		
+		Atm a = new Atm();
+		
+		Thread t1 = new Thread(a);
+		Thread t2 = new Thread(a);
+		Thread t3 = new Thread(a);
+		
+		t1.setName("Kuber");
+		t2.setName("moskov");
+		t3.setName("Argus");
+		
+		t1.start();
+		t2.start();
+		t3.start();
+
+		
+	}
+
+}
